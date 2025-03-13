5193b1ee0f790616d6e2719449c5b5798a559405
43343 <Fortyk2017@outlook.com>
2025-03-13 22:56:05 +0300

    client update

--- keychainer.cpp
+++
@@ -1,5 +0,0 @@
-#include "keychainer.h"
-
-extern QKeychain::ReadPasswordJob m_readCredentialJob;
-extern QKeychain::WritePasswordJob m_writeCredentialJob;
-extern QKeychain::DeletePasswordJob m_deleteCredentialJob;
